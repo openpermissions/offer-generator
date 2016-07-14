1561cbf9714bf7ff78334ab08f3d1fc5eb1eabdf
Tessa Conway <tessa.conway@cde.catapult.org.uk>
2016-07-14 15:04:57 +0100

    Allow assigner to be specified

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,40 +18,45 @@ const {combineReducers} = require('redux'),
       OfferTemplate = require('../template'),
       Immutable = require('immutable');
 
-const _template = new OfferTemplate();
 
-const template = (state, action) => {
-  switch (action.type) {
-    case 'GENERATE_OFFER':
-      let offer = JSON.parse(_template.constructOffer());
-      action.onCreate(offer);
-      return state;
+const templateReducer = (assigner) => {
+  const _template = new OfferTemplate(assigner);
 
-    case 'UPDATE_ATTRIBUTE':
-      _template.updateAttribute(action.attrType, action.key, action.value);
-      return Immutable.fromJS(_template.toJS());
+  return (state, action) => {
+    switch (action.type) {
+      case 'GENERATE_OFFER':
+        let offer = JSON.parse(_template.constructOffer());
+        action.onCreate(offer);
+        return state;
 
-    case 'ADD_ENTITY':
-      _template.addEntity(action.parent, action.attrType, action.key, action.id);
-      return Immutable.fromJS(_template.toJS());
+      case 'UPDATE_ATTRIBUTE':
+        _template.updateAttribute(action.attrType, action.key, action.value);
+        return Immutable.fromJS(_template.toJS());
 
-    case 'REMOVE_ENTITY':
-      _template.removeEntity(action.parent, action.key, action.id);
-      return Immutable.fromJS(_template.toJS());
+      case 'ADD_ENTITY':
+        _template.addEntity(action.parent, action.attrType, action.key, action.id);
+        return Immutable.fromJS(_template.toJS());
 
-    case 'UPDATE_CONSTRAINT':
-      _template.updateConstraint(action.id, action.key, action.attrType, action.value);
-      return Immutable.fromJS(_template.toJS());
+      case 'REMOVE_ENTITY':
+        _template.removeEntity(action.parent, action.key, action.id);
+        return Immutable.fromJS(_template.toJS());
 
-    default:
-      return Immutable.fromJS(_template.toJS());
+      case 'UPDATE_CONSTRAINT':
+        _template.updateConstraint(action.id, action.key, action.attrType, action.value);
+        return Immutable.fromJS(_template.toJS());
+
+      default:
+        return Immutable.fromJS(_template.toJS());
+    }
   }
 };
 
 
-const app = combineReducers({
-  template,
-  //jsonld
-});
+const app = (assigner) => {
+  const template = templateReducer(assigner);
+  return combineReducers({
+    template
+  });
+};
 
 export default app
